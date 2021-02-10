38eac84543bb1928dc97d4933bc6de2ea3fc255e
Daniel Mathiot <d.danymat@gmail.com>
2021-02-10 18:07:34 +0100

    feat: Added options to recognize tailwind in vscode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,13 @@
 <template>
   <div
-    class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
+    class=" p-6 max-w-sm  mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4"
   >
     <div class="flex-shrink-0">
       <img class="h-12 w-12" src="../assets/logo.png" alt="ChitChat Logo" />
     </div>
     <div>
-      <div class="text-xl font-medium text-black">ChitChat</div>
+
+      <div class="text-xl font-medium text-black">{{title}}</div>
       <p class="text-gray-500">{{ msg }}</p>
     </div>
   </div>
@@ -19,5 +20,6 @@ defineProps({
   msg: String,
 });
 
-const state = reactive({ count: 0 });
+
+const title = 'Chitchat'
 </script>
