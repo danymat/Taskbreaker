2b579ee63905a89a21aa7c0352f656a852afc01b
Daniel Mathiot <d.danymat@gmail.com>
2021-02-10 19:49:53 +0100

    feat: created Task Component and added bg to root

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="grid content-center justify-center gap-10 bg-gradient-to-br from-green-50 to-green-100 h-screen">
+    <Task title="Description de la tâche" context="Context1" project="Projet1"/>
+    <Task title="Description de la tâche" context="Context1" project="Projet1"/>
+    <Task title="Description de la tâche" context="Context1" project="Projet1"/>
+    <Task title="Description de la tâche" context="Context1" project="Projet1"/>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Task from "./components/Task.vue"
+
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
---
+++ src/components/Task.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-row p-2 max-w-sm bg-gradient-to-r items-center justify-evenly content-center from-red-500 to-red-300 rounded-md text-white shadow-md">
+    <h2 class="text-xl font-bolds truncate">{{ title }}</h2>
+    <div class="font-light">{{context}}</div>
+    <div class="">{{project}}</div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  title: String,
+  context: String,
+  project: String
+})
+</script>
