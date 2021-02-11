197c33d9c7045ce6c6b734534e4b20f67d1435b4
Daniel Mathiot <d.danymat@gmail.com>
2021-02-11 10:02:32 +0100

    Fix: Layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="bg-gray-800 h-64 justify-between items-center flex text-gray-100 p-3"
-  >
+  <div class="bg-gray-800 justify-between items-center flex text-gray-100 p-3">
     <div class="text-xl font-normal bg-gray-200 p-2 text-gray-800 rounded-md">
       Taskbreaker
     </div>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -2,10 +2,9 @@
   <div
     class="flex flex-row p-2 gap-4 max-w-sm bg-gradient-to-r items-center justify-evenly content-center from-gray-700 to-gray-800 rounded-tr-lg rounded-bl-lg text-gray-100 shadow-lg"
   >
-
     <h2 class="text-xl font-bolds truncate">{{ title }}</h2>
-    <div class="font-light">{{ context }}</div>
-    <div class="">{{ project }}</div>
+    <div class="font-light truncate">{{ context }}</div>
+    <div class="truncate">{{ project }}</div>
   </div>
 </template>
 
