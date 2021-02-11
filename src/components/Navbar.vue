58d45bf2a877054face43f92aae34e8cb283c026
Daniel Mathiot <d.danymat@gmail.com>
2021-02-11 09:50:03 +0100

    feat: Added navbar content

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="bg-gray-800 h-64 flex text-gray-100">Navbar Content</div>
+  <div
+    class="bg-gray-800 h-64 justify-between items-center flex text-gray-100 p-3"
+  >
+    <div class="text-xl font-normal bg-gray-200 p-2 text-gray-800 rounded-md">
+      Taskbreaker
+    </div>
+    <div class="text-md font-light">Login</div>
+  </div>
 </template>
 <script setup>
 </script>
