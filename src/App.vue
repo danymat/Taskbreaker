77181198415126e5bf4a71996c2cd40828bf3aec
Daniel Mathiot <d.danymat@gmail.com>
2021-02-15 15:21:36 +0100

    feat: Added button to add new task

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
-    <div class="flex flex-col h-screen bg-gradient-to-br from-gray-200 to-gray-300">
-        <Navbar class="shadow-2xl" />
-        <Menu class="" />
-        <div class="grid content-center justify-center gap-2 pt-6">
-            <div v-for="x in 4">
-                <Task title="Description de la tâche"
-                      context="Context1"
-                      project="Projet1" />
-            </div>
-        </div>
+  <div
+    class="flex flex-col h-screen bg-gradient-to-br from-gray-200 to-gray-300"
+  >
+    <Navbar class="shadow-2xl" />
+    <!-- <Menu class="" /> -->
+    <div class="grid content-center justify-center gap-2 pt-6">
+      <NewButton @click="createTask" class="mb-2" />
+      <div v-for="task in tasks" v-bind:key="task">
+        <Task
+          :title="task.title"
+          :context="task.context"
+          :project="task.project"
+          :notification="task.hasNotification"
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-    import Task from "./components/Task.vue";
-    import Navbar from "./components/Navbar.vue";
-    import Menu from "./components/Menu.vue";
+import Task from "./components/Task.vue";
+import Navbar from "./components/Navbar.vue";
+// import Menu from "./components/Menu.vue";
+import NewButton from "./components/NewButton.vue";
+import { ref } from "vue";
+var tasks = ref([]);
 
-    // This starter template is using Vue 3 experimental <script setup> SFCs
-    // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+const hasNotification = false; // Display notification on top of task
+const createTask = () => {
+  tasks.value.push({
+    title: "hey",
+    context: "hey2",
+    project: "project",
+    hasNotification: "true",
+  });
+};
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
---
+++ src/components/NewButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button class="flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      class="fill-current text-white-600 h-6 w-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+      />
+    </svg>
+    <span class="ml-2 text-white-600 text-sm">New Task</span>
+  </button>
+</template>
+
+<script setup>
+</script>
