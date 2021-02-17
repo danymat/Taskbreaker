88a0cf3db784a588b9455d96f2317d1a6807e0c9
Daniel Mathiot <d.danymat@gmail.com>
2021-02-17 21:20:48 +0100

    feat: UI Changes

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,16 +1,24 @@
 <template>
-    <div class="">
-        <h1 class="text-6xl">Welcome to Taskbreaker</h1>
-        <h2 class="text-3xl">The breaking task manager</h2>
-        <img :src="'/sprites/knight/idle ('+img_number+').png'" width="130" height="157"/>
+  <div class="text-center flex flex-row">
+    <div>
+      <img
+        :src="'/sprites/knight/idle (' + img_number + ').png'"
+        width="130"
+        height="157"
+      />
     </div>
+    <div class="grid place-content-center">
+      <h2 class="text-xl">Gamify your tasks with</h2>
+      <h1 class="text-6xl font-bold">Taskbreaker</h1>
+    </div>
+  </div>
 </template>
 <script setup>
-    import { ref } from "vue";
-    const img_number = ref(1);
-    setInterval(change_img, 100);
+import { ref } from "vue";
+const img_number = ref(1);
+setInterval(change_img, 100);
 
-    function change_img() {
-        img_number.value = (img_number.value)%10 +1
-    }
+function change_img() {
+  img_number.value = (img_number.value % 10) + 1;
+}
 </script>
