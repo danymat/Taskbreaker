16fa3f2ea38da8ef8220c10f31e57cd9f3386b01
Daniel Mathiot <d.danymat@gmail.com>
2021-02-18 01:08:54 +0100

    feat: Added task in home page

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="text-center flex flex-col">
+  <div class="text-center flex flex-col space-y-10">
     <Headline />
+    <div class="flex">
+      <NewButton @click="createTask" class="mb-2 flex" />
+    </div>
+
+    <div class="space-y-2">
+      <div v-for="task in tasks" v-bind:key="task">
+        <Task
+          :title="task.title"
+          :context="task.context"
+          :project="task.project"
+          :notification="task.hasNotification"
+        />
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import Headline from "./Headline.vue";
-const img_number = ref(1);
-setInterval(change_img, 100);
+import NewButton from "./NewButton.vue";
+import Task from "./Task.vue";
 
-function change_img() {
-  img_number.value = (img_number.value % 10) + 1;
-}
+var tasks = ref([]);
+const img_number = ref(1);
+
+const createTask = () => {
+  console.log("here");
+  tasks.value.push({
+    title: "Lorem Ipsum Dolor",
+    context: "Lorem",
+    project: "Lorem Ipsum Dolor",
+    hasNotification: false,
+  });
+};
 </script>
