aae8fe413456df86a3b4e29f7c65a5b223ee1f09
Marc Antoine Raymond <marc.raymond@insa-cvl.fr>
2021-03-06 19:30:27 +0100

    next actions in main

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,7 +1,71 @@
 <template>
     <div class="text-center flex flex-col space-y-10">
-        <p>MAIN PAGE taskboard shows all tasks this one only next ones maybe ?</p>
+        <div class="flex flex-row space-x-10">
+            <Tasklist :title="nextlist.title"
+                      :tasks="nextlist.tasks"
+                      @sort="(value) => update_sort(value.sort_value, value.tasks)" />
+        </div>
     </div>
 </template>
 <script setup>
+    import { ref } from "vue";
+    import Task from "./Task.vue";
+    import Tasklist from "./Tasklist.vue";
+
+    var nextlist = ref({});
+    nextlist.value = {
+        "title": "Next Actions",
+        "tasks": {}
+    };
+
+    var sort_value = ref("");
+    sort_value.value = "manual";
+
+    // sorting
+    const update_sort = (sorter, tasks) => {
+        sort_value.value = sorter;
+        if (sorter != "manual") {
+            if (sorter == "priority")
+                tasks.sort(task_sorter_priority);
+            else
+                if (sorter == "completion_date")
+                    tasks.sort(task_sorter_completion);
+                else
+                    if (sorter == "creation_date")
+                        tasks.sort(task_sorter_creation);
+        }
+
+    };
+
+    const task_sorter_priority = (a, b) => {
+        console.log(a, b)
+        if (a.priority != "" && b.priority != "") {
+            if (a.priority < b.priority)
+                return -1;
+            if (a.priority > b.priority)
+                return 1;
+        }
+        if (a.priority != "" && b.priority == "") return -1
+        if (b.priority != "" && a.priority == "") return 1
+        if (b.priority == "" && a.priority == "") return 1
+        return 0;
+    };
+
+    const task_sorter_completion = (a, b) => {
+        if (a.completion_date < b.completion_date)
+            return -1;
+        if (a.completion_date > b.completion_date)
+            return 1;
+
+        return 0;
+    };
+
+    const task_sorter_creation = (a, b) => {
+        if (a.creation_date < b.creation_date)
+            return -1;
+        if (a.creation_date > b.creation_date)
+            return 1;
+
+        return 0;
+    };
 </script>
