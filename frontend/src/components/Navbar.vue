2a985be97dc7b131e2695b27e93bbaaabe6e0622
Daniel Mathiot <d.danymat@gmail.com>
2021-03-10 20:05:40 +0100

    fix: layout fixes in navbarwq

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="justify-between flex p-3">
+  <div class="justify-between flex p-5">
     <button
       @click="changeMenuState"
-      class="text-xl font-medium p-2 text-gray-00 rounded-md"
+      class="text-xl font-medium text-gray-00 rounded-md"
     >
       Taskbreaker
     </button>
-    <div class="text-md font-light mr-2">
-      <router-link to="/signin" tag="button" class="p-2" v-if="!store.getters.isLoggedIn">
+    <div class="text-md font-light space-x-4">
+      <router-link to="/signin" tag="button" v-if="!store.getters.isLoggedIn">
           Sign In
       </router-link>
-      <router-link to="/signup" tag="button" class="p-2" v-if="!store.getters.isLoggedIn">
+      <router-link to="/signup" tag="button" v-if="!store.getters.isLoggedIn">
           Sign up
       </router-link>
-      <button class="p-2" v-if="store.getters.isLoggedIn" v-on:click="logout">
+      <button v-if="store.getters.isLoggedIn" v-on:click="logout">
           Log out
       </button>
     </div>
