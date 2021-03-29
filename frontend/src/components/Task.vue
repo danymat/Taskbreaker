25d9b241d969c1b7777e9d5aab552145b4b3bf01
Marc Antoine Raymond <marc.raymond@insa-cvl.fr>
2021-03-29 09:23:46 +0200

    task tooltip

--- frontend/src/components/Task.vue
+++ frontend/src/components/Task.vue
@@ -1,12 +1,5 @@
 <template>
-    <div v-if="show" class="flex relative flex-row p-2 gap-2 bg-gradient-to-r items-center justify-evenly content-center from-gray-600 to-gray-700 rounded-2xl text-gray-50 shadow-lg">
-        <div v-if="priority">
-            <div class="absolute font-extralight text-tiny">priority</div>
-            <h2 class="ml-2 mt-2 font-bolds text-xs">{{ priority.toString() }}</h2>
-        </div>
-        <div v-if="createdDate">
-            <h2 class="ml-2 mt-2 text-xs">{{ getDateDay() }}/{{ getDateMonth() }}/{{ getDateYear() }}</h2>
-        </div>
+    <div v-if="show" class="group flex relative flex-row p-2 gap-2 bg-gradient-to-r items-center justify-evenly content-center from-gray-600 to-gray-700 rounded-2xl text-gray-50 shadow-lg">
         <div>
             <h2 class="text-l ml-2 font-bolds text-sm">{{ description }}</h2>
         </div>
@@ -15,9 +8,25 @@
                 <h2 class="font-light ml-1 text-xs">@{{ context }}</h2>
             </div>
         </div>
-        <div v-if="project">
-            <div class="absolute font-extralight text-tiny">Project</div>
-            <h2 class="ml-2 mt-2 text-xs">{{ project }}</h2>
+        <div class="invisible group-hover:visible
+             absolute 
+             left-1/4 bottom-full z-10 
+             flex flex-row p-2 gap-2 
+             bg-gradient-to-r from-gray-200 to-gray-300
+             items-center justify-evenly content-center 
+             rounded-2xl 
+             text-black font-semibold shadow-lg">
+            <div v-if="priority">
+                <div class="absolute text-tiny">priority</div>
+                <h2 class="ml-2 mt-2 font-bolds text-xs">{{ priority.toString() }}</h2>
+            </div>
+            <div v-if="createdDate">
+                <h2 class="ml-2 mt-2 text-xs">{{ getDateDay() }}/{{ getDateMonth() }}/{{ getDateYear() }}</h2>
+            </div>
+            <div v-if="project">
+                <div class="absolute text-tiny">Project</div>
+                <h2 class="ml-2 mt-2 text-xs">{{ project }}</h2>
+            </div>
         </div>
         <div v-if="completionDate">
             COMPLETED
@@ -25,7 +34,8 @@
     </div>
 </template>
 <script setup>
-    import { defineProps } from "vue";
+    import { defineProps, defineEmit } from "vue"
+
     var props = defineProps({
         priority: Number,
         createdDate: Number,
@@ -35,6 +45,7 @@
         project: "",
         show: Boolean
     });
+
     const getDateDay = () => {
         var date = new Date(props.createdDate)
         return date.getDate()
